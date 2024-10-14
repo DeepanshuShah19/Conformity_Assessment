1495c0e330585a893ceefee5a83d38b59ae708c4
Deepanshu Shah <deepanshushah@deepanshus-air.home>
2024-10-14 01:52:24 -0400

    created a get api call using express to get all data from mongodb database

--- conformality_backend/app.js
+++ conformality_backend/app.js
@@ -1,11 +1,32 @@
 const mongoose = require('mongoose');
+const express = require('express');
 
-const mongoURI = 'mongodb+srv://deepanshuyshah:PdFzEAZxH0qrCfPB@cluster0.giwsj.mongodb.net/';
+const app = express();
+const mongoURI = 'mongodb+srv://deepanshuyshah:PdFzEAZxH0qrCfPB@cluster0.giwsj.mongodb.net/Conformity';
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 mongoose.connect(mongoURI)
-.then(() => {
-    console.log('Connected to MongoDB databse');
-})
-.catch((err) => {
-    console.error('Failed to connect to MongoDB:', err);
+    .then(() => {
+        console.log('Connected to MongoDB databse');
+    })
+    .catch((err) => {
+        console.error('Failed to connect to MongoDB:', err);
+    });
+
+const defSchema = new mongoose.Schema({}, { strict: false });
+const Database = mongoose.model("customerData", defSchema, "customerData");
+
+app.get('/data', async (req, res) => {
+    try {
+        const data = await Database.find();
+        console.log("data received from database: ", data)
+        res.json(data);
+    } catch (err) {
+        console.error('Error fetching data:', err);
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
 });
